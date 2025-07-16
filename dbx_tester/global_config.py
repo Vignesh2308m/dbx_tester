ad6f27c3ebc1c98cae2d7f8e9b7a52d5705b47c1
Vignesh2308m <Vignesh2308m>
2025-07-16 10:28:26 +0000

    Modified global config

--- dbx_tester/global_config.py
+++ dbx_tester/global_config.py
@@ -1,3 +1,8 @@
-class GlobalConfigs:
-    REPO_PATH = None
-    
+class GlobalConfig:
+    _instance = None
+
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super(GlobalConfig, cls).__new__(cls)
+            cls._instance.REPO_PATH = False
+        return cls._instance
