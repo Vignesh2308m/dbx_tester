0b101d8344124d18892308fc56f7434f69ec2845
Vignesh Manoharan <vignesh.manoharan@perficient.com>
2025-08-12 13:16:38 +0530

    Jobs TestSuite Started

--- dbx_tester/jobs.py
+++ dbx_tester/jobs.py
@@ -1,6 +1,8 @@
 from dbx_tester.global_config import GlobalConfig
 from dbx_tester.utils.databricks_api import *
 from pathlib import Path
+import json
+
 
 
 class job_test():
@@ -16,13 +18,14 @@ class job_test():
         if self.is_test:
             self.test_cache_path = self.global_config.TEST_CACHE_PATH / self.current_path.relative_to(self.global_config.TEST_PATH).parent / '_notebook_test_cache'
             self.notebook_dir = self.test_cache_path / self.current_path.name
+            self.config_dir = self.notebook_dir / 'config'
             
             self._create_files_and_folders()
             self._transform_notebook()
         else:
             self.test_cache_path = Path(*self.current_path.parts[:self.current_path.parts.index("_notebook_test_cache")+1])
             self.notebook_dir = self.current_path.parent
-            
+            self.config_dir = self.notebook_dir / 'config'
 
         pass
 
@@ -33,6 +36,7 @@ class job_test():
 
         self.test_cache_path.mkdir(exist_ok=True, parents=True)
         self.notebook_dir.mkdir(exist_ok=True, parents=True)
+        self.config_dir.mkdir(exist_ok=True, parents=True)
 
         pass
 
@@ -40,7 +44,17 @@ class job_test():
         """
         This function will transform the notebook to be run in the test cache
         """
-        
+        test_notebook = create_notebook(self.fn.__name__)
+
+        test_notebook + create_cell(f"%run {self.current_path}")
+
+        test_notebook + create_cell(f"{self.fn.__name__}().run()")
+
+        test_notebook.save_notebook(self.notebook_dir / self.fn.__name__)
+
+        with open(self.config_dir / f"{self.fn.__name__}.json", "w") as f:
+                json.dump(self.config, f)
+                
         pass
 
 class job_test_runner():
