28339f84cfcad0e17f354a888665e2a9ae6bff67
vicky.manoharan2018@gmail.com <vicky.manoharan2018@gmail.com>
2025-08-20 10:23:14 +0000

    Updates

--- dbx_tester/notebook.py
+++ dbx_tester/notebook.py
@@ -71,7 +71,7 @@ class notebook_test():
 
         test_notebook.add_cell(f"{self.fn.__name__}().run()")
 
-        test_notebook.save_notebook(self.notebook_dir / self.fn.__name__)
+        test_notebook.save_notebook((self.notebook_dir / self.fn.__name__).as_posix())
 
         pass
 
