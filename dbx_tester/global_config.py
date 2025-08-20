b3a357fdb5953817d40cb048e588ec049eca79b7
vicky.manoharan2018@gmail.com <vicky.manoharan2018@gmail.com>
2025-08-20 10:10:04 +0000

    Global Config bug fix

--- dbx_tester/global_config.py
+++ dbx_tester/global_config.py
@@ -1,3 +1,4 @@
+
 import json
 import os
 from pathlib import Path
@@ -6,89 +7,64 @@ from pathlib import Path
 class GlobalConfig:
     def __init__(self):
         self.config_path = Path('/Workspace/Shared') / "dbx_tester_cfg.json"
-        self._REPO_PATH = None
-        self._TEST_PATH = None
-        self._TEST_CACHE_PATH = None
-        self._LOG_PATH = None
-        self._CLUSTER_ID = None
-        
-        if not os.path.exists(self.config_path):
-            with open(self.config_path, "w") as f:
-                json.dump({}, f)
+        self._config = {}
+        self._load_config()
+
+    def _load_config(self):
+        """Load configuration from file or create empty config if file doesn't exist."""
+        if self.config_path.exists():
+            with open(self.config_path, "r") as f:
+                self._config = json.load(f)
+        else:
+            self._config = {}
+            self._save_config()
+
+    def _save_config(self):
+        """Save the current configuration to file."""
+        with open(self.config_path, "w") as f:
+            json.dump(self._config, f, indent=2)
 
     @property
     def REPO_PATH(self):
-        if os.path.exists(self.config_path):
-            with open(self.config_path, "r") as f:
-                config = json.load(f)
-                return config.get("REPO_PATH")
-        return self._REPO_PATH
+        return self._config.get("REPO_PATH")
 
     @REPO_PATH.setter
     def REPO_PATH(self, value):
-        self._REPO_PATH = value
-        self.save()
+        self._config["REPO_PATH"] = value
+        self._save_config()
 
     @property
     def TEST_PATH(self):
-        if os.path.exists(self.config_path):
-            with open(self.config_path, "r") as f:
-                config = json.load(f)
-                return config.get("TEST_PATH")
-        return self._TEST_PATH
+        return self._config.get("TEST_PATH")
 
     @TEST_PATH.setter
     def TEST_PATH(self, value):
-        self._TEST_PATH = value
-        self.save()
+        self._config["TEST_PATH"] = value
+        self._save_config()
 
     @property
     def TEST_CACHE_PATH(self):
-        if os.path.exists(self.config_path):
-            with open(self.config_path, "r") as f:
-                config = json.load(f)
-                return config.get("TEST_CACHE_PATH")
-        return self._TEST_CACHE_PATH
+        return self._config.get("TEST_CACHE_PATH")
 
     @TEST_CACHE_PATH.setter
     def TEST_CACHE_PATH(self, value):
-        self._TEST_CACHE_PATH = value
-        self.save()
+        self._config["TEST_CACHE_PATH"] = value
+        self._save_config()
 
     @property
     def LOG_PATH(self):
-        if os.path.exists(self.config_path):
-            with open(self.config_path, "r") as f:
-                config = json.load(f)
-                return config.get("LOG_PATH")
-        return self._LOG_PATH
+        return self._config.get("LOG_PATH")
 
     @LOG_PATH.setter
     def LOG_PATH(self, value):
-        self._LOG_PATH = value
-        self.save()
-    
-    def save(self):
-        config = {
-            "REPO_PATH": self._REPO_PATH,
-            "TEST_PATH": self._TEST_PATH,
-            "TEST_CACHE_PATH": self._TEST_CACHE_PATH,
-            "LOG_PATH": self._LOG_PATH
-        }
-        with open(self.config_path, "w") as f:
-            json.dump(config, f)
-    
+        self._config["LOG_PATH"] = value
+        self._save_config()
+
     @property
     def CLUSTER_ID(self):
-        if os.path.exists(self.config_path):
-            with open(self.config_path, "r") as f:
-                config = json.load(f)
-                return config.get("CLUSTER_ID")
-        return self._CLUSTER_ID
-    
+        return self._config.get("CLUSTER_ID")
+
     @CLUSTER_ID.setter
     def CLUSTER_ID(self, value):
-        self._CLUSTER_ID = value
-        self.save()
-    
-    
+        self._config["CLUSTER_ID"] = value
+        self._save_config()
