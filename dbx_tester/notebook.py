e85e6f42fef7c8f1db340fddee507febc4ab008b
Vignesh Manoharan <vignesh.manoharan@perficient.com>
2025-09-09 12:54:05 +0530

    notebook changed

--- dbx_tester/notebook.py
+++ dbx_tester/notebook.py
@@ -299,7 +299,8 @@ class NotebookTest:
                 task_key=task,
                 notebook_path=task_path.as_posix(),
                 cluster_id=node.cluster or self.cluster_id,
-                depends_on=edges if edges else None
+                depends_on=edges if edges else None,
+                params={"trigger_run": "true"}
             )
         
         logger.info(f"Created submission: {submission.as_dict()}")
